978220b1524c7bc2ca5977f11f99f410d3c2f88f
Ha Nguyen <nguyenttha89@gmail.com>
2018-07-16 19:12:35 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,5 +2,5 @@ var playlist = {Alison: "Slowdive", Sometimes: "My Bloody Valentine"};
 
 function updatePlaylist(playlist, artistName, songTitle)
 {
-  
+  return Object.assign({}, playlist, {[artistName]: songTitle})
 }
